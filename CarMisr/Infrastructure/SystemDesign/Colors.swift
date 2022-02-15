823cc299b2b518599200d928ffc575462da0c023
abumandour <muhammadabumandour@outlook.com>
2022-02-15 22:02:41 +0200

    init system design

---
+++ CarMisr/Infrastructure/SystemDesign/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  CarMisr
+//
+//  Created by Muhammad Abumandour on 15/02/2022.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor{
+    static var mainBackgroundColor = UIColor(named: "mainBackgroundColor")!
+    static var mainFontColor = UIColor(named: "mainFontColor")!
+}
