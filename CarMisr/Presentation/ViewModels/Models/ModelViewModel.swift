b3ae4d3b633524d369257ec1f21170f7b66a4dee
abumandour <muhammadabumandour@outlook.com>
2022-02-17 05:24:39 +0200

    implement model view controller and view model

--- CarMisr/Presentation/ViewModels/Models/ModelViewModel.swift
+++ CarMisr/Presentation/ViewModels/Models/ModelViewModel.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+final class ModelViewModel{
+    // MARK: - Properties
+    private var disposeBag = DisposeBag()
+    private var models = PublishRelay<[Model]>()
+    private var tempModels = [Model]()
+    private var isLoading = BehaviorRelay<Bool>(value: false)
+    private var isLoadMore = BehaviorRelay<Bool>(value: false)
+    private var canLoadMore = true
+    private var pageNumber = 0
+    private var itemsCount = 0
+    
+    struct Input {
+        let didAppear: Driver<Void>
+        let modelSelected: ControlEvent<Model>
+        let prefetchRows: ControlEvent<[IndexPath]>
+    }
+    
+    struct Output{
+        let models: Observable<[Model]>
+        let isloading: Observable<Bool>
+        let isloadMore: Observable<Bool>
+    }
+    
+    //MARK: - Initailizer
+    init() { }
+        
+    //MARK: - Public Method
+    func transform(input: Input) -> Output{
+        
+        input.didAppear
+            .drive { [weak self] (_) in
+                guard let self = self else { return }
+                self.loadData()
+            }.disposed(by: disposeBag)
+        
+        input.modelSelected
+            .bind { [weak self] model in
+                guard let self = self else { return }
+                self.showModelDetails(model: model)
+            }.disposed(by: disposeBag)
+        
+        input.prefetchRows
+            .bind { [weak self] indexPaths in
+                guard let self = self else { return }
+                self.loadMore(prefetchRowsAt: indexPaths)
+            }.disposed(by: disposeBag)
+        
+        return Output(models: models.asObservable(), isloading: isLoading.asObservable(), isloadMore: isLoadMore.asObservable())
+    }
+    
+    //MARK: - Internal Method
+    private func loadData(){
+        Task{
+            isLoading.accept(true)
+            tempModels.removeAll()
+            pageNumber = 0
+            canLoadMore = true
+            await requestMakes()
+            isLoading.accept(false)
+        }
+    }
+        
+    private func loadMore(prefetchRowsAt indexPaths: [IndexPath]){
+        for index in indexPaths {
+            if index.row >= itemsCount - 3 && canLoadMore && !isLoading.value {
+                isLoadMore.accept(true)
+                pageNumber += 1
+                Task{ await requestMakes() }
+                isLoadMore.accept(false)
+                break
+            }
+        }
+    }
+    
+    private func requestMakes() async{
+        try? await Task.sleep(nanoseconds: 3000000000)
+        models.accept([Model]())
+    }
+    
+    private func showModelDetails(model: Model){
+        
+    }
+
+}
--- CarMisr/Presentation/Views/Models/ModelViewController.swift
+++ CarMisr/Presentation/Views/Models/ModelViewController.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ModelViewController: UIViewController {
+class ModelViewController: UIViewController , UITableViewDelegate{
 
     //MARK: - Constants
     let cellIdentifier = "modelViewCell"
     
     //MARK: - Properties
     @IBOutlet weak var modelsTableView: UITableView!
-        
+    @IBOutlet weak var mainActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var loadMoreActivityIndicator: UIActivityIndicatorView!
+    private let modelViewModel: ModelViewModel!
+    private let disposeBag = DisposeBag()
     //MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +27,47 @@ class ModelViewController: UIViewController {
         setupMakeTableView()
     }
     
+    //MARK: - Initializer
+    init(modelViewModel: ModelViewModel) {
+        self.modelViewModel = modelViewModel
+        super.init(nibName: "ModelViewController", bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     //MARK: - Internal Method
     private func setupMakeTableView(){
         modelsTableView.register(ModelTableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        modelsTableView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+    
+    private func bind(){
+        let viewDidAppear = rx.sentMessage(#selector(UIViewController.viewDidAppear(_:))).take(1).mapToVoid().asDriverComplete()
+        let prefetchDataSource = modelsTableView.rx.prefetchRows
+        let input = ModelViewModel.Input(didAppear: viewDidAppear,
+                                         modelSelected: modelsTableView.rx.modelSelected(Model.self),
+                                         prefetchRows: prefetchDataSource)
+        let outup = modelViewModel.transform(input: input)
+        outup.models
+            .bind(to: modelsTableView.rx.items(cellIdentifier: cellIdentifier, cellType: ModelTableViewCell.self)) { (row, model, cell) in
+                cell.configure(.init(model: model))
+            }
+            .disposed(by: disposeBag)
+        outup.models
+            .map ({ $0.count <= 0})
+            .distinctUntilChanged()
+            .bind(to: modelsTableView.rx.isEmpty(message: "No data"))
+            .disposed(by: disposeBag)
+        outup.isloading
+            .observe(on: MainScheduler.instance)
+            .map(!)
+            .bind(to: mainActivityIndicator.rx.isHidden)
+            .disposed(by: disposeBag)
+        outup.isloadMore
+            .observe(on: MainScheduler.instance)
+            .map(!)
+            .bind(to: loadMoreActivityIndicator.rx.isHidden)
+            .disposed(by: disposeBag)
     }
 }
